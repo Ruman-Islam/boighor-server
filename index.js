17aae35c34fc6e0e104f0d1634b8aaee8e35397c
Ruman-Islam <rumanislam48@gmail.com>
2022-08-12 17:54:08 +0600

    server database activated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -5,14 +6,32 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-//? Middleware
+// Middleware //
 app.use(cors());
 app.use(express.json());
-
+// ...................... //
 
 // ? Database connection configuration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vzdnu.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true },
+    () => console.log('connected to db!'))
+// ? ...................... //
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port, () => {
     console.log('Server is running on:', port);
 })
+
+console.log(__dirname)
+console.log(__filename)
